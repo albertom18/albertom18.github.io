9e29da4947fd7284e7434b913a67237a529e10fd
albertom18 <juanalberto180797@gmail.com>
2025-05-05 14:34:06 -0400

    Add files via upload

---
+++ Website.js
@@ -0,0 +1,30 @@
+const checkbox = document.querySelectorAll("#Recruit");
+
+checkbox[0].onclick = function(){
+    alert("Outside Hitter was clicked");
+};
+
+checkbox[1].onclick = function(){
+    alert("Opposite Hitter was clicked");
+};
+
+checkbox[2].onclick = function(){
+    alert("Setter was clicked");
+};
+
+checkbox[3].onclick = function(){
+    alert("Middle Blocker was clicked");
+};
+
+checkbox[4].onclick = function(){
+    alert("Libero was clicked");
+};
+
+checkbox[5].onclick = function(){
+    alert("Defensive Specialist was clicked");
+};
+
+checkbox[6].onclick = function(){
+    alert("Serving Specialist was clicked");
+};
+
